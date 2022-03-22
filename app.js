f9fe4b2a96e35dc4fad8be7cf10e204ea4bccc76
Josh Petersen <jdpetersen92@gmail.com>
2022-03-22 08:47:01 -0700

    finished memory game

--- app.js
+++ app.js
@@ -48,16 +48,66 @@ const cardArray = [
         img: './assets/yoda.jpg'
     }
 ]
-
 cardArray.sort(() => 0.5 - Math.random())
 
+let cardsChosen = []
+let cardsChosenIds = []
+const cardsWon = []
+let attempts = 0
+
+const result = document.querySelector('#result')
+const tries = document.querySelector('#tries')
+
 const grid = document.querySelector('#grid')
+
+
 
 const createBoard = () => {
     for (let i = 0; i < cardArray.length; i++) {
         const card = document.createElement('img')
-        card.setAttribute('scr', 'assets/blank.png')
-        card.
+        card.setAttribute('src', './assets/logo.png')
+        card.setAttribute('data-id', i)
+        card.addEventListener('click', flipCard)
+        grid.append(card)
+    }
+}
+
+const checkMatch = () => {
+
+    const cards = document.querySelectorAll('#grid img')
+    const optionOneId = cardsChosenIds[0]
+    const optionTwoId = cardsChosenIds[1]
+    if(optionOneId === optionTwoId) {
+        alert('you already clicked that image')
+    }
+    if (cardsChosen[0] === cardsChosen[1]){
+        cards[optionOneId].removeEventListener('click', flipCard)
+        cards[optionTwoId].removeEventListener('click', flipCard)
+        cardsWon.push(cardsChosen)
+    } else {
+        cards[optionOneId].setAttribute('src', './assets/logo.png')
+        cards[optionTwoId].setAttribute('src', './assets/logo.png')
+    }
+    cardsChosen = []
+    cardsChosenIds = []
+    result.innerHTML = cardsWon.length
+    attempts = attempts + 1
+    tries.innerHTML = attempts
+
+    if(cardsWon.length === cardArray.length/2) {
+        alert('You Matched them all')
+        
+    }
+}
+
+
+function flipCard() {
+    const cardId = this.getAttribute('data-id')
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenIds.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if (cardsChosen.length === 2) {
+        setTimeout(checkMatch, 500)
     }
 }
 
